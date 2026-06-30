ee27608fb11346b9494682e8ccb84702e397422a
ahmadasshidiq <ahmadasshidiq08@gmail.com>
2026-06-30 09:24:47 +0700

    [AY] modify: location types on finance

--- app/(dashboard)/finance/components/account-dialog.tsx
+++ app/(dashboard)/finance/components/account-dialog.tsx
@@ -11,7 +11,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { AccountCategory, AccountFormState } from "./types";
+import { AccountCategory, AccountFormState } from "../types";
 
 type Props = {
   open: boolean;
--- app/(dashboard)/finance/components/partner-dialog.tsx
+++ app/(dashboard)/finance/components/partner-dialog.tsx
@@ -5,7 +5,7 @@ import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } f
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { PartnerFormState } from "./types";
+import { PartnerFormState } from "../types";
 
 type Props = {
   open: boolean;
